935287c213b24bef1a067c33f95e0ad4842c4c53
Judah Sleibi <judahsleibi34@gmail.com>
2026-05-30 00:11:46 +0300

    Front end fixing and restpassword adding the CEO will be PROUD! Cuase I fixed the session issues he could not fix part 2

--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, Response, Request
+from fastapi import APIRouter, HTTPException, Request, Response
 
 from database import service_supabase, supabase
 from classes import SignUpRequest, LogIn, UpdatePassword
